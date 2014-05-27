f80d084972168e6a70c4195a28f459e08f59e08d
Zac Staples <zacstaples@mac.com>
2014-05-26 23:14:45 -0700

    Working Version
    Added a shell script to clean up and a few system commands
    to open the result file in my browser.

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm tmp.html
+rm front_page.html
+rm cdma.html
+rm list_items.html
--- gr_scraper.cpp
+++ gr_scraper.cpp
@@ -35,6 +35,10 @@
 
 using namespace std;
 
+// initiate libcurl globally
+curl::global g;
+
+
 // error() disguises throws:
 inline void error(const string& s){ throw runtime_error(s);}
 
@@ -54,7 +58,7 @@ void write_to_file(const string& file_name, const string& data ) {
 }
 
 void curl_get(const string url, const string file) {
-  	curl::global g;
+//  	curl::global g;
   	string s;
   	curl::callback::string cb(s);
   	curl::handle h(g, cb);
@@ -69,7 +73,45 @@ void curl_get(const string url, const string file) {
   	}
 }
 
-void process_front_page(const string in_file, const string out_file){
+
+void append_to_file(const string& file_name, const string& data ) 
+{
+	static bool first_write = true;
+	ofstream ofs;
+	if(first_write) {
+		ofs.open(file_name.c_str());
+		first_write = false;
+	}else {
+		ofs.open(file_name.c_str(), ofstream::app );
+	}	
+	if(!ofs) error("can't open the output file to write cdma_lines stream");
+	ofs<<data<<endl;
+	cout<<"appended to "<<file_name<<endl;
+}
+
+void add_headers(ofstream& ofs)
+{
+	ofs<<"<html>\n";
+	ofs<<"<head>\n";
+	ofs<<"<title>Zac's GNU-Radio scraper</title>\n";
+	ofs<<"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n";
+	ofs<<"</head><body><h2>Zac's GNU-Radio Scraper</h2>\n";
+	ofs<<"<h4>All discussion of CDMA since 2001</h4>\n";	
+}
+
+void add_footers(ofstream& ofs) 
+{
+	ofs<<"<hr>\n";
+	ofs<<"<em>This software is provided free of charge under the beer license."
+	   <<" Under the terms of the beer license you owe me a beer at the appropriate"
+	   <<" point in the not too distant future.</em>\n";
+	ofs<<"</body></html>\n";
+	
+}
+
+void process_front_page(const string in_file, const string out_file,
+						const string base_url)
+{
 	//read the file in one line at a time and write it to the output file
 	//open the input file
 	ifstream ifs(in_file.c_str());
@@ -83,19 +125,121 @@ void process_front_page(const string in_file, const string out_file){
 	string line;
 	while(ifs) {
 		getline(ifs, line);	
-		string p = "^<li>.+=(.+)>.+</li>$";
+		string p = "^<li>.+=\"(.+)\">.+</li>$";
 		boost::regex pat(p);
 		boost::smatch matches;
 		if(boost::regex_match(line, matches, pat)) {
 			if(matches.size() == 2){		//we have a match for the capture
-				ofs<<matches[1]<<endl;	//add the capture to the links file
+				ofs<<base_url<<matches[1]<<endl;	//add the capture to the links file
 			}
 		}
 	}
 }
 
+void process_tmp_file(const string in_file, const string out_file) 
+{
+	//step through each line of the in_file and then append strings
+	//to the cdma_links when you get a match
+	
+	//open the input file
+	ifstream ifs(in_file.c_str());
+	if(!ifs) error("can't open the tmp_file from list_items");
+	
+	//read each line and write it to output if it mentions CDMA
+	string line;
+	while(ifs) {
+		getline(ifs, line);	
+		string p = ".*(cdma|CDMA).*";
+		boost::regex pat(p);
+		boost::smatch matches;
+		if(boost::regex_match(line, matches, pat)) {
+			cout<<"cdma match\n";
+			append_to_file(out_file, line);
+		}
+	}
+}
+
+
+
+void process_cdma_links(const string in_file, const string out_file) 
+{
+	cout<<"Processing final html doc\n";
+	
+	string base_url = "http://lists.gnu.org/archive/html/discuss-gnuradio/";
+	string subj_url;
+	string msg_url;
+	string year_url;
+	string month_url;
+	string closing_url;	
+	//read the file in one line at a time and write it to the output file
+	//open the input file
+	ifstream ifs(in_file.c_str());
+	if(!ifs) error("can't open the input file to process the front page");
+	//open the output file
+	ofstream ofs(out_file.c_str());
+	if(!ofs) error("can't open the output file to process the front page");
+	
+	add_headers(ofs);
+	
+	//read each line, reformat the href links and write to output
+	//default line looks like this 
+	//<li><b><a name="00180" href="msg00180.html">Re: [Discuss-gnuradio] CDMA-UMTS protocol implementation with USRP1</a></b>, <i>Marcus Müller</i>, <tt>2014/05/09</tt>
+	//we want to grab the date at the end and reformat like this
+	//<li><b><a name="00180" href="http://lists.gnu.org/archive/html/discuss-gnuradio/2014-05/msg00180.html">Re: [Discuss-gnuradio] CDMA-UMTS protocol implementation with USRP1</a></b>, <i>Marcus Müller</i>, <tt>2014/05/09</tt>
+	
+	string line;
+	while(ifs) {
+		getline(ifs, line);	
+		string p = ".*(msg\\d+.html)(\".*<tt>)(\\d{4})/(\\d{2})(.*)";
+		boost::regex pat(p);
+		boost::smatch matches;
+		if(boost::regex_match(line, matches, pat)) {
+			if(matches.size() == 6 ){ 		//we have matches for message, year and month
+				msg_url = matches[1];
+				subj_url = matches[2];
+				year_url = matches[3];
+				month_url = matches[4];
+				closing_url = matches[5];
+			}
+			ofs<<"<li><b><a href=\""<<base_url<<year_url<<"-"<<month_url
+				<<'/'<<msg_url<<subj_url<<year_url<<'/'<<month_url<<closing_url;
+		}
+	}
+	add_footers(ofs);
+	
+}
+
+void curl_get_then_append(const string url, const string tmp_file,
+						  const string cdma_file) 
+{
+//  	curl::global g;
+  	static string s;
+  	static curl::callback::string cb(s);
+  	static curl::handle h(g, cb);
+//	curl::tag a;
+  	try {
+	  	h.get(url);
+		write_to_file(tmp_file, s);
+		process_tmp_file(tmp_file, cdma_file);
+		cb.clear();
+  	} catch (const underflow_error &e) {
+		cout<<e.what()<<endl;
+		cout << "error caught in curl_get_then_append\n";
+  	}
+}
+
+bool is_URL(const string line){
+	/*
+		TODO rudimentary test right now on length...need regex here too 
+	*/
+	
+	if (line.size() > 5) return true;
+	return false;
+}
+
 int main(int argc, char **argv) {
-	try {	
+	try {
+	  		
 		// curl the front page and store results in a file
 		curl_get("http://lists.gnu.org/archive/html/discuss-gnuradio/",
 				 "front_page.html"
@@ -103,8 +247,31 @@ int main(int argc, char **argv) {
 				
 		// iterate over the front page saving every list item in the page 
 		// to another file
-		process_front_page("front_page.html", "list_items.html");
-	}
+		process_front_page("front_page.html", "list_items.html", 
+						   "http://lists.gnu.org/archive/html/discuss-gnuradio/"
+						   );
+						
+		//now I need to build a curl loop 
+		ifstream ifs("list_items.html");
+		if(!ifs) error("can't open the input file to process first curl loop");
+		string line;
+		while(ifs) {
+			getline(ifs, line);
+			if(is_URL(line))
+				curl_get_then_append(line, "tmp.html", "cdma.html");
+		}
+		
+		// now that I have all the CDMA related articles I just need to 
+		// reformat the cdma.html page with correct fully defined links in href line
+		process_cdma_links("cdma.html", "cdma_final.html");
+		
+		int i;
+		if (system(NULL)) puts("Opening cdma_final.html");
+		else error("system is unabailable to open cdma_final.html");
+		i=system("open cdma_final.html");
+		i=system("./clean.sh");
+}
+
 	catch(runtime_error& e){
 		cerr<<"runtime_error: "<<e.what()<<endl;
 		return 5;
